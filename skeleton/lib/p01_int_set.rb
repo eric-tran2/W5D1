2859a2d99952af3618c5e99e179cebbbfbe2e39e
Eric Tran <eric.tran119@gmail.com>
2021-05-17 15:08:09 -0400

    resizing intset

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,22 +1,33 @@
 class MaxIntSet
+  attr_reader :store
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    is_valid?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    is_valid?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    raise "Out of bounds" if num < 0 || num > @max
   end
 
   def validate!(num)
+
   end
 end
 
@@ -27,18 +38,26 @@ class IntSet
   end
 
   def insert(num)
+    if !self.include?(num)
+      self[num] << num
+    end
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets] 
   end
 
   def num_buckets
@@ -55,18 +74,24 @@ class ResizingIntSet
   end
 
   def insert(num)
+    count += 1
+    if !self.include?(num)
+      self[num] << num
+    end
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets] 
   end
 
   def num_buckets
@@ -74,5 +99,8 @@ class ResizingIntSet
   end
 
   def resize!
+    if @count == num_buckets
+      @store = Array.new(@count * 2) { Array.new }
+    end
   end
 end
