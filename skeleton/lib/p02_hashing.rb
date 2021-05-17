0cb4b0a02c564b310ec8f9bd2cbe3f816d84d2a2
Eric Tran <eric.tran119@gmail.com>
2021-05-17 17:44:10 -0400

    second part

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    result = 0
+    return self.empty?.hash if self.empty?
+    (0...self.length-1).each do |i|
+      result += (self[i].hash ^ self[i+1].hash)
+    end
+    result
   end
 end
 
 class String
   def hash
+    result = 0
+    chars = self.split("").permutations
+    (0...chars.length-1).each do |i|
+      result += (chars[i].hash ^ chars[i+1].hash)
+    end
+    result
   end
 end
 
